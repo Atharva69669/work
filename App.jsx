1c925e3ef268d03f249bc16dacfaea286dd3579d
Atharva Bilonikar <116868631+Atharva69669@users.noreply.github.com>
2025-03-20 23:21:26 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,53 +1,27 @@
 import React from "react";
-import { JsonForms } from "@jsonforms/react";
-import { materialRenderers, materialCells } from "@jsonforms/material-renderers";
-import { Paper, Box, Typography } from "@mui/material";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import schema from "./schema.json";
-import uischema from "./uischema.json";
-import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import FixedLabelTextField from "./FixedLabelTextField";
 
-function App()
-{
-  const [data, setData] = useState({});
-  const theme=createTheme({
-    components: {
-      MuiInputBase: {
-        styleOverrides: {
-          root: {
-            backgroundColor: "#f9f9f9",
-            borderRadius: "8px",
-            padding: "10px",
-          },
-          input: {
-            color: "#333",
-          },
-        },
-      },
-      MuiFormLabel: {
-        styleOverrides: {
-          root: {
-            color: "#666",
-            fontWeight: "bold",
-          },
-        },
-      },
-    },
-  });
+function App() {
   return (
-    <div>
-      <h2>Dynamic Form using JSON Forms</h2>
-      <ThemeProvider theme={theme}>
-      <Box sx={{ maxWidth: "600px", margin: "auto", padding: "20px" }}>
-        <Paper sx={{ padding: "20px", borderRadius: "12px", boxShadow: 3 }}>
-          <Typography variant="h5" gutterBottom>
-            Loan Application Form
-          </Typography>
-          <JsonForms schema={schema} uischema={uischema} renderers={materialRenderers} cells={materialCells} />
-        </Paper>
-      </Box>
-    </ThemeProvider>
-    </div>
+    <>
+      {/* 
+     label: Outlined (Specifies the floating label text that appears inside the text field, moves up when focused)
+     variant="outlined"
+     Defines the style of the text field.
+     Possible values:
+     outlined → Default, input field with a border.
+     filled → Background-filled field.
+     standard → No border, only underline.
+    */}
+
+
+      {/* <TextField id="outlined-basic" label="Outlined" variant="outlined"/> */}
+
+      <FixedLabelTextField/>
+
+
+    </>
   );
 }
 
