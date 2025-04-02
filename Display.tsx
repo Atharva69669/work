317dc22286cf98e6a3fd8fdb6756ccf8cbf73055
Atharva Bilonikar <116868631+Atharva69669@users.noreply.github.com>
2025-04-02 19:33:19 +0530

    Update Display.tsx

--- Display.tsx
+++ Display.tsx
@@ -12,44 +12,42 @@ export const Display: React.FC = () => {
   ];
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
-      <h2>Component Comparison</h2>
+    <div style={{ padding: "20px", maxWidth: "800px", margin: "auto" }}>
+      <h2 style={{ textAlign: "center" }}>Component Comparison</h2>
 
-      {/* Custom Dropdown */}
-      <div>
-        <h3>Custom Dropdown</h3>
-        <CustomDropDown
-          label="Custom Dropdown"
-          options={options}
-          value={customValue}
-          onChange={(e) => setCustomValue(e.target.value)}
-        />
-      </div>
+      <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", gap: "40px" }}>
+        {/* Left: Custom Dropdown */}
+        <div style={{ flex: 1 }}>
+          <h3 style={{ textAlign: "center" }}>Custom Dropdown</h3>
+          <CustomDropDown
+            label="Custom Dropdown"
+            options={options}
+            value={customValue}
+            onChange={(e) => setCustomValue(e.target.value)}
+          />
+        </div>
 
-      {/* Broadridge Dropdown */}
-      <div>
-        <h3>Broadridge Dropdown</h3>
-        <FormControl style={{ width: "250px" }}>
-          <InputLabel>Broadridge Dropdown</InputLabel>
-          <Select
-            value={broadridgeValue}
-            onChange={(e) => setBroadridgeValue(e.target.value)}
-          >
-            <MenuItem value="" disabled>
-              None
-            </MenuItem>
-            {options.map((option) => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.label}
+        {/* Right: Broadridge Dropdown */}
+        <div style={{ flex: 1 }}>
+          <h3 style={{ textAlign: "center" }}>Broadridge Dropdown</h3>
+          <FormControl style={{ width: "100%" }}>
+            <InputLabel>Broadridge Dropdown</InputLabel>
+            <Select
+              value={broadridgeValue}
+              onChange={(e) => setBroadridgeValue(e.target.value)}
+            >
+              <MenuItem value="" disabled>
+                None
               </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+              {options.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
       </div>
-
-      {/* Additional Components can be added here */}
-      
     </div>
   );
 };
-
