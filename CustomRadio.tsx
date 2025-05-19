b30055453a74969236bced0912a6df3e0c4fcd2d
Atharva Bilonikar <116868631+Atharva69669@users.noreply.github.com>
2025-05-19 11:53:10 +0530

    Update CustomRadio.tsx

--- CustomRadio.tsx
+++ CustomRadio.tsx
@@ -1,7 +1,9 @@
+import * as React from 'react';
 import Radio from '@mui/material/Radio';
+import FormControlLabel from '@mui/material/FormControlLabel';
 import { styled } from '@mui/material/styles';
 
-const CustomRadio = styled(Radio)(({ theme }) => ({
+const StyledRadio = styled(Radio)(({ theme }) => ({
   color: '#1c1c1c',
   '&.Mui-checked': {
     color: '#1c1c1c',
@@ -9,11 +11,24 @@ const CustomRadio = styled(Radio)(({ theme }) => ({
   '&:hover': {
     backgroundColor: 'transparent',
     boxShadow: 'none',
-  }
+  },
 }));
 
-export default function Example() {
+type CustomRadioProps = {
+  label: string;
+  value: string;
+  checked?: boolean;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const CustomRadio: React.FC<CustomRadioProps> = ({ label, value, checked, onChange }) => {
   return (
-    <CustomRadio />
+    <FormControlLabel
+      value={value}
+      control={<StyledRadio checked={checked} onChange={onChange} />}
+      label={label}
+    />
   );
-}
+};
+
+export default CustomRadio;
